d509e34a2da520b7f1063ee4cea948e9b3a212b1
Muneeb Zain <muneebzain008@gmail.com>
2026-02-28 02:42:43 +0500

    Initial Commit

---
+++ AIKnowledgeAssistant/AIKnowledgeAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIKnowledgeAssistantApp.swift
+//  AIKnowledgeAssistant
+//
+//  Created by Muneeb Zain on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AIKnowledgeAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
